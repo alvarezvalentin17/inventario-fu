1243f0013a3ae25e0f544d25fb9a57e6b0977cb0
alvarezvalentin17 <valen.alvarez27@icloud.com>
2022-09-11 19:35:06 -0300

    avances con crear nuevos documentos

--- src/common/modal/modal.jsx
+++ src/common/modal/modal.jsx
@@ -17,17 +17,21 @@ function Modal({
     <>
     {state &&
         <Overlay viewOverlay={viewOverlay} positionModal={positionModal}>
+
             <ContenedorModal padding={padding}>
+
                 {viewHeader &&
-                <EncabezadoModal>
-                    <h3 className='title'>{title}</h3>
-                </EncabezadoModal>
+                    <EncabezadoModal>
+                        <h3 className='title'>{title}</h3>
+                    </EncabezadoModal>
                 }
                     <BotonCerrar onClick={()=>{changeState(!state)}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/></svg>
                     </BotonCerrar>
-                         {children}
+
+                {children}
+
             </ContenedorModal>
         </Overlay>
     }
@@ -51,7 +55,7 @@ const Overlay = styled.div`
 `;
 
 const ContenedorModal = styled.div `
-    width: 600px;
+    width: 500px;
     min-height: 100px;
     background: #fff;
     position: relative;
--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,8 +1,44 @@
-import React, {useState} from 'react'
-import Modal from '../../common/modal/modal'
+import React, {useState} from 'react';
+import Modal from '../../common/modal/modal';
+import { collection, addDoc } from "firebase/firestore";
+import db from "../../services/firestore";
+import './table.css';
 
 function Table({items}) {
   const [stateModal1, setStateModal1] = useState(false)
+  const [sistemaOperativo, setSistemaOperativo] = useState()
+  const [mark, setMark] = useState()
+  const [name_pc, setName_PC] = useState()
+  const [model, setModel] = useState()
+
+  const saveDataItems = () =>{
+
+    let so = document.getElementById('so');
+    setSistemaOperativo(so.value);
+
+    let mark = document.getElementById('mark');
+    setMark(mark.value);
+
+    let name_pc = document.getElementById('name_pc');
+    setName_PC(name_pc.value);
+
+    let model = document.getElementById('model');
+    setModel(model.value);
+
+    }
+
+  const addItems = async () => {
+    const docRef = await addDoc(collection(db, "items"), {
+      name_pc: name_pc,
+      mark: mark,
+      so: sistemaOperativo,
+      model: model,
+    });
+  }
+
+ const closeModal = () => {
+  setStateModal1(!stateModal1)
+ }
 
   return (
     <>
@@ -40,7 +76,7 @@ function Table({items}) {
                 
             </tbody>
         </table>
-      {/* Modal  */}
+      {/* Modal - Crear nuevo item */}
       <Modal
           title='Editar'
           state={stateModal1}
@@ -49,40 +85,68 @@ function Table({items}) {
           viewOverlay={true}
           padding={'20px'}
        >
-          <div className='container'>
-            <div className='mb-3 form-group'>
-              <label>Usuario: </label>
-              <input className='d-block form-control' type="text" />
-            </div>
-            <div className='mb-3 form-group'>
-              <label>Nombre de PC: </label>
-              <input className='d-block form-control'type="text" />
-            </div>
-            <div className='mb-3 form-group'>
-              <label>Modelo: </label>
-              <input className='d-block form-control' type="text" />
-            </div>
-            <div className='mb-3 form-group'>
-              <label>Procesador: </label>
-              <input className='d-block form-control'type="text" />
-            </div>
-            <div className='mb-3 form-group'>
-              <label>RAM: </label><input className='d-block form-control'type="text" />
-            </div>
-            <div className='mb-3 form-group'>
-              <label>Precio: </label>
-              <input className='d-block form-control'type="text" />
-            </div>
-            <div className='mb-3 form-group'>
-              <label>Sistema Operativo: </label>
-              <input className='d-block form-control'type="text" />
-            </div>
-            <div className='mb-3 form-group'>
-              <label>Tipo: </label>
-              <input className='d-block form-control'type="text" />
-            </div>
-            <button type='button' className='btn btn-primary' onClick={()=>{setStateModal1(!stateModal1)}}>Aceptar</button>
+     <div class="container text-center">
+        <div class="row align-items-start">
+          <div class="col">
+            <label className='me-3'>Nombre y Apellido: </label>
+            <input onChange={saveDataItems} id='user' type="text" />
           </div>
+          <div class="col">
+            <label id='name_pc' className='me-3'>Nombre de PC: </label>
+            <input onChange={saveDataItems} type="text" />
+          </div>
+          <div class="col">
+          <label className='me-3'>Marca: </label>
+            <select class="input" id='mark' onChange={saveDataItems} aria-label="Default select example">
+              <option value="DELL">DELL</option>
+              <option value="Lenovo">Lenovo</option>
+            </select>
+          </div>
+          <div class="col">
+            <label className='me-3'>Modelo: </label>
+            <input onChange={saveDataItems} id='model' type="text" />
+          </div>
+          <div class="col">
+            <label className='me-3'>Procesador: </label>
+            <input onChange={saveDataItems} id='processor' type="text" />
+          </div>
+          <div class="col">
+            <label className='me-3'>RAM: </label>
+            <input onChange={saveDataItems} id='memory_ram' type="text" />
+          </div>
+          <div class="col">
+            <label className='me-3'>Pantalla: </label>
+            <input onChange={saveDataItems} type="text" />
+          </div>
+          <div class="col">
+            <label className='me-3'>Precio: </label>
+            <input onChange={saveDataItems} id='display' type="text" />
+          </div>
+          <div class="col">
+            <label className='me-3'>Serial: </label>
+            <input onChange={saveDataItems} id='serial' type="text" />
+          </div>
+          <div class="col">
+            <label className='me-3'>Sistema Operativo: </label>
+            <select id='so' onChange={saveDataItems} className="input " aria-label="Default select example">
+                <option value="Windows 10 x64">Windows 10 x64</option>
+                <option value="Windows 10 x32">Windows 10 x32</option>
+                <option value="Windows 8 x64">Windows 8 x64</option>
+                <option value="Windows 8 x32">Windows 8 x32</option>
+                <option value="Windows 7 x64">Windows 7 x64</option>
+                <option value="Windows 7 x32">Windows 7 x32</option>
+                <option value="Windows XP x64">Windows XP x64</option>
+                <option value="Windows XP x32">Windows XP x32</option>
+              </select>
+            </div>
+        </div>
+            <button type='button' className='btn btn-primary mt-3' onClick={()=>{
+              closeModal()
+              addItems()
+            }}>Aceptar</button>
+      </div>
+         
+          
         </Modal>
 
     </div>
